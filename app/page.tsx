a0dbe1521ab62bb4cc0236043eaef3953f562570
abhishekibr2 <abhishekibr.trainee2@gmail.com>
2025-07-26 11:41:32 +0530

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/tutorial/sign-up-user-steps";
+import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
+import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
